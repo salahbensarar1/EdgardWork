d83cc9a25fbb785a5d4ce4810c0caf1ae531e93d
salah ben sarar <sa.bensarar@gmail.com>
2024-06-16 22:40:10 +0200

    Initial Commit

---
+++ Edgard2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Edgard2
+//
+//  Created by salahbensarar on 2024. 06. 16..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
